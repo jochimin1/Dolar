4069bda28cde2162b26031b4100ed09dbba4e3bf
jochimin1 <jochimin.contreras@unicaribe.edu.do>
2022-08-30 13:37:13 -0400

    add comments

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# imports 
 import requests
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
 
+# instance FastAPI
 app = FastAPI()
 
 
+# url to scrape
 url_dolar = "http://hoy.com.do"
 
-
+# request to url
 response = requests.get(url_dolar)
 
+# parse all the html
 html = BeautifulSoup(response.text, "html.parser")
 
-dolar_compra = html.find_all('td')[1].text
 
+# scrape a specific section of html
+dolar_compra = html.find_all('td')[1].text
 dolar_venta = html.find_all('td')[2].text
 
 #print(' $$$$Precio Dolar$$$$$')
 #print('\nPrecio Compra->', dolar_compra)
 #print('Precio Venta->', dolar_venta)
 
-
-@app.get('/compra')
+# Route to show results
+@app.get('/dolar')
 async def root():
     return {
         "Compra Dolar": dolar_compra,
